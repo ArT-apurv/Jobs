16ab636a4fdd94fdc16a3862e1656debd76877f9
ArT-apurv <89764232+ArT-apurv@users.noreply.github.com>
2022-05-24 16:05:30 +0530

    added login routes

--- app.js
+++ app.js
@@ -3,20 +3,25 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const connectDB = require("./Database/Connect");
+
+const authRouter = require("./Routes/authRoutes");
+const jobsRouter = require("./Routes/jobsRoutes");
+
 const notFoundMiddleware = require("./Middleware/not-found");
 const errorHandlerMiddleware = require("./Middleware/error-handler");
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("jobs api");
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
